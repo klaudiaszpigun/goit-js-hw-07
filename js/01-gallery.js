d54de7f79c2fea8d659a3f2e2114e535047b234d
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-01-27 12:08:33 +0100

    new

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// zmienna galerii w html
+const gallery = document.querySelector(".gallery");
+
+// iteracja po tablicy
+const listItems = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+        <img
+          class="gallery__image"
+          src="${galleryItem.preview}"
+          data-source="${galleryItem.original}"
+          alt="${galleryItem.description}">
+        </img>
+      </a>
+    </li>`
+  )
+  .join("");
+
+// elementy zostały już dodane do HTML
+gallery.insertAdjacentHTML("afterbegin", listItems);
+
+// usuwamy ustawienia przeglądaki do całej galerii
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+});
