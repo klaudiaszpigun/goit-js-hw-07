04de146239188d0fefe27960b91f2bf42e51b9bb
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-01-27 14:45:09 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,52 +1,69 @@
+// importowanie tablicy obrazów
 import { galleryItems } from "./gallery-items.js";
 
-//zmienna za pomocą, której dostajemy się do galeri
+//zmienna za pomocą, której dostajemy się do galerii
 const gallery = document.querySelector(".gallery");
 
+// zmienna listItems to iteracja po tablicy obrazów i za pomocą wartości właściwości danego elementu iterowanego, tworzony jest kod HTML
 const listItems = galleryItems
   .map(
+    // parametr galleryItem oznacza iterowany element
     (galleryItem) =>
       `<li class="gallery__item">
             <a class="gallery__link" href="${galleryItem.original}">
                 <img
-            class="gallery__image"
-            src="${galleryItem.preview}"
-            data-source="${galleryItem.original}"
-            alt="${galleryItem.description}"
-            >
+                  class="gallery__image"
+                  src="${galleryItem.preview}"
+                  data-source="${galleryItem.original}"
+                  alt="${galleryItem.description}"
+                >
             </a>
-    </li>`
+      </li>`
   )
   .join("");
 
+// dodanie do galerii którą jest znacznik <ul></ul> kod HTML, który powstał przy pomocy iteracji po tablicy
 gallery.insertAdjacentHTML("afterbegin", listItems);
 
-let activeLightbox;
+// deklarujemy zmienną activeLightbox która obecnie ma wartość null
+let activeLightbox = null;
 
-// Obsługa kliknięć na obrazki w galerii
+// gdy klikniemy na galerię wywołuje się callback
 gallery.addEventListener("click", (event) => {
+  // usuwa domyślne ustawienia przegllądarki
   event.preventDefault();
 
+  // tworzymy zmienną za pomocą której łatwiej będzie nam się dostać do elementu na którym został wywołany event
   const clickedElement = event.target;
+  // jeśli element na którym został wywołany event to nie jest zdjęcie
   if (clickedElement.nodeName !== "IMG") {
+    // zakończ działanie programu
     return;
   }
+  // w przeciwnym przypadku
 
+  // stwórz zmienną która zawiera wartość zmiennej data-source klikniętego elementu
   const largeImageUrl = clickedElement.dataset.source;
 
-  const lightbox = basicLightbox.create(`
-    <img src="${largeImageUrl}" class="gallery__image">
-  `);
+  // zmienna lightbox zawiera utworzony za pomocą biblioteki modal
+  const lightbox = basicLightbox.create(
+    // w src danego zdjęcia podana jest zmienna largeImageUrl króra zawiera ścieżkę do obrazu
+    `<img src="${largeImageUrl}" class="gallery__image">`
+  );
+  // uruchomienie zmiennej lightbox
   lightbox.show();
 
+  // przypisanie do zmiennej activeLightbox, aktywny lightbox
   activeLightbox = lightbox;
 });
 
+// gdy nastąpi keydown
 document.addEventListener("keydown", (event) => {
+  // i jeśli klucz eventu to przycisk esc oraz okno lightbox będzie aktywne
   if (event.key === "Escape" && activeLightbox) {
-    // Zamknij otwarte okno Lightbox
+    // zamknij otwarte okno Lightbox
     activeLightbox.close();
-    // Zresetuj zmienną przechowującą otwarte okno Lightbox
+    // zresetuj zmienną przechowującą otwarte okno Lightbox
     activeLightbox = null;
   }
 });
