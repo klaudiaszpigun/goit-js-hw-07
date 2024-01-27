82f332bfccef04d4c155f7e08307bcdd30ee31c4
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-01-27 13:24:08 +0100

    jupi

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,52 @@
 import { galleryItems } from "./gallery-items.js";
 
-// zmienna galerii w html
+//zmienna za pomocą, której dostajemy się do galeri
 const gallery = document.querySelector(".gallery");
 
-// iteracja po tablicy
 const listItems = galleryItems
   .map(
     (galleryItem) =>
       `<li class="gallery__item">
-      <a class="gallery__link" href="${galleryItem.original}">
-        <img
-          class="gallery__image"
-          src="${galleryItem.preview}"
-          data-source="${galleryItem.original}"
-          alt="${galleryItem.description}">
-        </img>
-      </a>
+            <a class="gallery__link" href="${galleryItem.original}">
+                <img
+            class="gallery__image"
+            src="${galleryItem.preview}"
+            data-source="${galleryItem.original}"
+            alt="${galleryItem.description}"
+            >
+            </a>
     </li>`
   )
   .join("");
 
-// elementy zostały już dodane do HTML
 gallery.insertAdjacentHTML("afterbegin", listItems);
 
-// usuwamy ustawienia przeglądaki do całej galerii
+let activeLightbox;
+
+// Obsługa kliknięć na obrazki w galerii
 gallery.addEventListener("click", (event) => {
   event.preventDefault();
+
+  const clickedElement = event.target;
+  if (clickedElement.nodeName !== "IMG") {
+    return;
+  }
+
+  const largeImageUrl = clickedElement.dataset.source;
+
+  const lightbox = basicLightbox.create(`
+    <img src="${largeImageUrl}" class="gallery__image">
+  `);
+  lightbox.show();
+
+  activeLightbox = lightbox;
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape" && activeLightbox) {
+    // Zamknij otwarte okno Lightbox
+    activeLightbox.close();
+    // Zresetuj zmienną przechowującą otwarte okno Lightbox
+    activeLightbox = null;
+  }
 });
